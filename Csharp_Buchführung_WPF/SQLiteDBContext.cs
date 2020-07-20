29dce97026418b70d47f7dabe56b25858ab50bef
Ludwig Ettner <ludwig.ettner@hotmail.de>
2020-07-20 16:41:55 +0200

    Datenbank SQLight wurde hinzugefügt. Es fehlt noch der Test, ob korrekt konfiguriert

---
+++ Csharp_Buchführung_WPF/SQLiteDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Csharp_Buchführung_WPF
+{
+    class SQLiteDBContext : DbContext
+    {
+        public DbSet<Projekt> Projekte { get; set; }
+        public DbSet<Stufe> Stufen { get; set; }
+        public DbSet<Eintrag> Einträge { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("Data Source=BtmDB.db");
+    }
+}
